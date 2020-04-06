b81114b04ed658c2453072972ba4613b20a7f00b
ianroberson <35911724+ianroberson@users.noreply.github.com>
2020-04-06 11:46:19 -0600

    Update restaurant_filter.py

--- restaurant_filter.py
+++ restaurant_filter.py
@@ -9,11 +9,11 @@ restaurants = []
 for index, value in enumerate(data):
     if('Food' in str(data[index]['categories'])):
         restaurants.append(data[index])
-    if('food' in str(data[index]['categories'])):
+    elif('food' in str(data[index]['categories'])):
         restaurants.append(data[index])
-    if('Restaurant' in str(data[index]['categories'])):
+    elif('Restaurant' in str(data[index]['categories'])):
         restaurants.append(data[index])
-    if('restaurant' in str(data[index]['categories'])):
+    elif('restaurant' in str(data[index]['categories'])):
         restaurants.append(data[index])
 
 with open('restaurants.json','w') as restaurant_file:
