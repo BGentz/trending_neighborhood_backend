b4e79b69a2a729874d45f68af91f3e21058bb555
ianroberson <ian.roberson@gmail.com>
2020-04-08 11:15:10 -0600

    Backend + Frontend connectivity

--- api/views.py
+++ api/views.py
@@ -1,13 +1,22 @@
 # from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
+import json
+from django.http import JsonResponse, HttpResponse
 
 
 @csrf_exempt
 def user_submit(request):
-    if request.method == "POST":  # if it's a post request
-        json = request.POST['json']
-        print(type(json))
-        print(json)
+    received = request.body
+    print(type(received))
+    print(received)
+    message_data = {'message': 'Test message'}
+    print(message_data)
 
-    else:
-        print('Get request')
+
+# b'{"city":"Chicago","categories":
+# {"accessability":0,"artsAndEntertainment":0,"bars":0,
+# "localEvents":3,"restaurants":0,"retail":2,"schools":0}}'
+
+    return JsonResponse(data=message_data, status=200)
+    # if request.method == "POST":
+    #     print(request.body)
--- trending_neighborhood/settings.py
+++ trending_neighborhood/settings.py
@@ -38,12 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
-    'corsheaders'
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -123,6 +124,15 @@ STATIC_URL = '/static/'
 
 
 CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ALLOW_HEADERS = True
+
+
+
+
+# CORS_ORIGIN_WHITELIST = [
+#     '*.*.*.*',
+# ]
+
 # CORS_ORIGIN_WHITELIST = [
 #     'http://localhost:3000',
 #     'http://localhost:8000',
